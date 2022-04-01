916a4d6fd24c3ff526e4ca2587598152a930ac06
Steve Korshakov <steve@korshakov.com>
2022-04-01 18:26:35 +0400

    feat: compilation

---
+++ scripts/build.sh
@@ -0,0 +1,43 @@
+set -e
+
+# Build amalgam
+mkdir -p ./output/
+rm -f ./output/dao.fc
+touch ./output/dao.fc
+
+# Constants
+cat ./source/modules/constants.fc >> ./output/dao.fc
+echo $'\n' >> ./output/dao.fc
+
+# Utils
+cat ./source/modules/utils.fc >> ./output/dao.fc
+echo $'\n' >> ./output/dao.fc
+
+# Storage
+cat ./source/modules/storage-base.fc >> ./output/dao.fc
+echo $'\n' >> ./output/dao.fc
+cat ./source/modules/storage-members.fc >> ./output/dao.fc
+echo $'\n' >> ./output/dao.fc
+cat ./source/modules/storage-proposals.fc >> ./output/dao.fc
+echo $'\n' >> ./output/dao.fc
+
+# Ops
+cat ./source/modules/ops.fc >> ./output/dao.fc
+echo $'\n' >> ./output/dao.fc
+
+# Main
+cat ./source/dao.fc >> ./output/dao.fc
+echo $'\n' >> ./output/dao.fc
+
+# Build contract
+
+ton-compiler \
+    --output ./output/dao.fif \
+    --input ./output/dao.fc
+ton-compiler \
+    --fift \
+    --output ./output/dao.cell \
+    --input ./output/dao.fif
+openssl base64 -A \
+    -in ./output/dao.cell  \
+    -out ./output/dao.cell.base64
