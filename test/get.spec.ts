486775783a688d1eba1ee84d22679212b9aa978d
Steve Korshakov <steve@korshakov.com>
2022-04-09 18:55:25 +0400

    fix: fix load_shares

---
+++ test/get.spec.ts
@@ -0,0 +1,32 @@
+import { beginCell, toNano } from "ton";
+import { SmartContract } from "ton-contract-executor";
+import { createCode } from "./contract/createCode";
+import { createData } from "./contract/createData";
+import { createMetadata, createProposal, createVote, executeProposal, getProposal } from "./contract/operations";
+import { sendMessage } from "./contract/sendMessage";
+import { randomAddress } from "./utils/randomAddress";
+
+const member1 = randomAddress(0, "initial-1");
+const member2 = randomAddress(0, "initial-2");
+const member3 = randomAddress(0, "initial-3");
+const member4 = randomAddress(0, "initial-4");
+const outsider = randomAddress(0, "outsider-1");
+
+describe("get", () => {
+    it("should execute get_membership", async () => {
+
+        // Create contract
+        const executor = await SmartContract.fromCell(
+            createCode(),
+            createData({
+                totalShares: 1000,
+                failureTreshold: 250,
+                successTreshold: 1000,
+                members: [{ address: member1, shares: 1000 }]
+            })
+        );
+
+        let res = await executor.invokeGetMethod('get_membership', [{ type: 'cell_slice', value: beginCell().storeAddress(member2).endCell().toBoc({ idx: false }).toString('base64') }])
+        console.warn(res);
+    });
+});
